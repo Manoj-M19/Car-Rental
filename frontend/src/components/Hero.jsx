218b22627534d2b6af11d6beb01ce01af7d43c36
Manoj-M19 <manojpavithra2004@gmail.com>
2025-09-26 19:04:15 +0000

    modified

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='h-screen flex-col items-center justify-center gap-14
+    bg-light text-center'>
+
+        <h1 className='text-4xl md:text-5xl font-semibold'> Luxury Cars on Rent</h1>
+      
+    </div>
+  )
+}
+
+export default Hero
