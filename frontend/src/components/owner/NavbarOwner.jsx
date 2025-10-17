fb8f75281557545e36eda3026abca7995991cc6b
Manoj-M19 <manojpavithra2004@gmail.com>
2025-10-17 20:15:05 +0000

    added Dashboard

--- frontend/src/components/owner/NavbarOwner.jsx
+++ frontend/src/components/owner/NavbarOwner.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const NavbarOwner = () => {
   const user = dummyUserData;
   return (
-    <div className="flex items-center justify-between px-6 md:px-10 py-4 text-gray-500 border-borderColor relative transition-all">
+    <div className="flex  items-center justify-between px-6 md:px-10 py-4 text-gray-500 border-borderColor relative transition-all">
       <Link to="/">
         <img src={assets.logo} alt="" className="h-7" />
       </Link>
