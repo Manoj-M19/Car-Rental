927753ea9036d86e54212a5a4bf6ef87a9dfec99
Manoj-M19 <manojpavithra2004@gmail.com>
2025-10-18 17:23:58 +0000

    modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
       <Route path='/cars' element={<Cars/>}/>
       <Route path='/my-bookings' element={<MyBookings/>}/>
       <Route path='/owner'element={<Layout/>}>
-      <Route index element={<Dashboard/>}/>
+      <Route path='/owner/Dashboard' element={<Dashboard/>}/>
       <Route path="add-car" element={<AddCar/>}/>
       <Route path="manage-cars" element={<ManageCars/>}/>
       <Route path="manage-bookings" element={<ManageBookings/>}/>
