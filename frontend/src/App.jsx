03b130da1a95163fc73da11ad202f51c86a7f6be
Manoj-M19 <manojpavithra2004@gmail.com>
2025-10-20 16:05:46 +0000

    modfied

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,50 @@
-import Navbar from './components/Navbar'
-import { useState} from "react";
-import { Routes,Route ,useLocation } from "react-router-dom";
-import  Home from './pages/Home'
-import CarDetails from './pages/CarDetails';
-import Cars from './pages/Cars'
-import MyBookings from './pages/MyBookings';
+import { useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import CarDetails from './pages/CarDetails';
+import Cars from './pages/Cars';
+import MyBookings from './pages/MyBookings';
+
 import Layout from './pages/owner/Layout';
-import Dashboard from './pages/owner/Dashboard'
-import AddCar from './pages/owner/AddCar'; 
+import Dashboard from './pages/owner/Dashboard';
+import AddCar from './pages/owner/AddCar';
 import ManageCars from './pages/owner/ManageCars';
 import ManageBookings from './pages/owner/ManageBookings';
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner');
 
-  const [showLogin,setShowLogin] = useState(false)
-  const isOwnerPath = useLocation().pathname.startsWith('/owner')
   return (
     <>
-     {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+      {/* Only show Navbar if not owner route */}
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
-     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/car-details/:id' element={<CarDetails/>}/>
-      <Route path='/cars' element={<Cars/>}/>
-      <Route path='/my-bookings' element={<MyBookings/>}/>
-      <Route path='/owner'element={<Layout/>}>
-      <Route path='/owner/Dashboard' element={<Dashboard/>}/>
-      <Route path="add-car" element={<AddCar/>}/>
-      <Route path="manage-cars" element={<ManageCars/>}/>
-      <Route path="manage-bookings" element={<ManageBookings/>}/>
+      <Routes>
+        {/* Public Routes */}
+        <Route path='/' element={<Home />} />
+        <Route path='/car-details/:id' element={<CarDetails />} />
+        <Route path='/cars' element={<Cars />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
 
-      </Route>
-     </Routes>
+        {/* Owner Routes (Nested) */}
+        <Route path='/owner' element={<Layout />}>
+          <Route index element={<Dashboard />} /> {/* default route for /owner */}
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='add-car' element={<AddCar />} />
+          <Route path='manage-cars' element={<ManageCars />} />
+          <Route path='manage-bookings' element={<ManageBookings />} />
+        </Route>
+      </Routes>
 
-     {!isOwnerPath && <Footer />}
+      {/* Only show Footer if not owner route */}
+      {!isOwnerPath && <Footer />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
