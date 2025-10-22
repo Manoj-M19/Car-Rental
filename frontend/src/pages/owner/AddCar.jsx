b2c80264e79f69ea0316a4402c624628a6f38e60
Manoj-M19 <manojpavithra2004@gmail.com>
2025-10-22 12:21:07 +0000

    modified

--- frontend/src/pages/owner/AddCar.jsx
+++ frontend/src/pages/owner/AddCar.jsx
@@ -118,7 +118,8 @@ const AddCar = () => {
             </select>
           </div>
         </div>
-        <div className="grid-grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           <div className="flex flex-col w-full">
             <label>Transmission</label>
             <select
