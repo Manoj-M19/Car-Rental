4bfd0211f63d42087a4687a271ac0829d0c70160
Manoj-M19 <manojpavithra2004@gmail.com>
2025-10-24 04:46:50 +0000

    modified

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,10 @@
 import express, { Router } from "express";
-import { registerUser } from "../controllers/userController";
+import { registerUser,loginUser } from "../controllers/userController.js";
 
 
 const userRouter = express.Router();
 
 userRouter.post('/register',registerUser)
-userRouter.post('/login' ,loginUser)
+userRouter.post('/login',loginUser)
 
 export default userRouter
