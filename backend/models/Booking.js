12aec50c3a8764b9dece45c8c4311e146cdc65e9
Manoj-M19 <manojpavithra2004@gmail.com>
2025-10-28 18:38:24 +0000

    modified

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const {objectId} = mongoose.Schema.Types
+const {ObjectId} = mongoose.Schema.Types
 
 const bookingSchema = new mongoose.Schema({
-  car:{type:objectId,ref:"Car",required:true},
-  user:{type:objectId,ref:"User",required:true},
-  owner:{type:objectId,ref:"User",required:true},
+  car:{type:ObjectId,ref:"Car",required:true},
+  user:{type:ObjectId,ref:"User",required:true},
+  owner:{type:ObjectId,ref:"User",required:true},
   pickupDate:{type:Date,required:true},
   returnDate:{type:Date,required:true},
   status:{type:String,enum:["pending","confirmed","cancelled"],default:"pending"},
