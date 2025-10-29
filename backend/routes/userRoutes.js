f12712c8d945cb9d170759abf96f9783aa6d1eb4
Manoj-M19 <manojpavithra2004@gmail.com>
2025-10-29 17:14:27 +0000

    modified

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { registerUser,loginUser, getUserData } from "../controllers/userController.js";
+import { registerUser,loginUser, getUserData, getCars } from "../controllers/userController.js";
 import { protect } from "../middleware/auth.js";
 
 
@@ -8,5 +8,6 @@ const userRouter = express.Router();
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 userRouter.get('/data',protect,getUserData)
+userRouter.get('/cars',getCars)
 
-export default userRouter
+export default userRouter;
