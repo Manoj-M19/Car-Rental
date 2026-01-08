158dcde1da72f5ad47969ee9c53baf2df3b84e26
Manoj-M19 <manojpavithra2004@gmail.com>
2026-01-08 19:18:25 +0000

    modified

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
 import Title from "../components/Title";
-import { assets, dummyMyBookingsData } from "../assets/assets";
+import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 const MyBookings = () => {
 
-  const currency = import.meta.env.VITE_CURRENCY
+  const {axios,user,currency} = useAppContext();
+
   const [bookings, setBookings] = useState([]);
 
   const fecthMyBookings = async () => {
-    setBookings(dummyMyBookingsData);
+  try {
+    const {data} = await axios.get('/api/bookings/user')
+    if(data.success){
+      setBookings(data.bookings)
+    }else{
+      toast.error(data.message)
+    }
+  } catch(error){
+    toast.error(error.message)
+  }
   };
 
   useEffect(() => {
-     fecthMyBookings();
-  }, []);
+    user && fecthMyBookings();
+  }, [user]);
   return (
     <div className="px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl">
       <Title
